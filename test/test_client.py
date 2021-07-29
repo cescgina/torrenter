ea558181bd8ba000450af807eac41cfc1439bd3c
jgilaber <joan.gilabert@bsc.es>
2021-07-29 15:00:26 +0200

    Add tentative tests for multi-file torrents

--- test/test_client.py
+++ test/test_client.py
@@ -4,7 +4,8 @@ from bitstring import BitArray
 
 from . import no_logging
 from torrenter.client import Piece, Block, PieceManager, REQUEST_SIZE
-from torrenter.torrent import Torrent
+from torrenter.client import calculates_files_in_piece
+from torrenter.torrent import Torrent, TorrentFile
 
 
 class PieceTests(unittest.TestCase):
@@ -14,7 +15,7 @@ class PieceTests(unittest.TestCase):
 
     def test_request_ok(self):
         blocks = [Block(0, offset, length=10) for offset in range(0, 100, 10)]
-        p = Piece(None, 0, blocks)
+        p = Piece(None, 0, blocks=blocks)
 
         block = p.next_request()
         missing_blocks = [b for b in p.blocks if b.status is Block.Missing]
@@ -30,7 +31,7 @@ class PieceTests(unittest.TestCase):
 
     def test_reset_block(self):
         blocks = [Block(0, offset, length=10) for offset in range(0, 100, 10)]
-        p = Piece(None, 0, blocks)
+        p = Piece(None, 0, blocks=blocks)
 
         p.block_received(10, b"")
 
@@ -45,8 +46,8 @@ class PieceManagerTests(unittest.TestCase):
 
     def test_bitfield(self):
         self.piece_manager.total_pieces = 5
-        p1 = Piece(None, 0, [])
-        p2 = Piece(None, 4, [])
+        p1 = Piece(None, 0, blocks=[])
+        p2 = Piece(None, 4, blocks=[])
         self.piece_manager.have_pieces = [p1, p2]
         self.assertEqual(b"\x88", self.piece_manager.bitfield)
 
@@ -71,3 +72,48 @@ class PieceManagerTests(unittest.TestCase):
         self.assertTrue(len(piece_manager.missing_pieces[-1].blocks), 16)
         for b in piece_manager.missing_pieces[-1].blocks:
             self.assertTrue(b.length, REQUEST_SIZE)
+
+class MultiFileContentsPieceTest(unittest.TestCase):
+    def setUp(self):
+        self.limits = [(0, 0, 41), (1, 41, 1108), (2, 1149, 380672528)]
+
+    def test_first_piece(self):
+        piece_length = 8388608
+        output = calculates_files_in_piece(self.limits, 0, piece_length)
+        golden = [(0, 41), (1, 1108), (2, 8387459)]
+        self.assertEqual(output, golden)
+
+    def test_multiple_pieces(self):
+        piece_length = 8388608
+        output = calculates_files_in_piece(self.limits, 0, piece_length)
+        golden = [(0, 41), (1, 1108), (2, 8387459)]
+        self.assertEqual(output, golden)
+        for i in range(1, 5):
+            output = calculates_files_in_piece(self.limits, i*piece_length,
+                                               (i+1)*piece_length)
+            golden = [(2, piece_length)]
+            self.assertEqual(output, golden)
+
+class MultiFileContentsTorrentTest(unittest.TestCase):
+    def setUp(self):
+        torrent = Torrent("test/data/multi-file.torrent")
+        self.piece_manager = PieceManager(torrent)
+
+    def test_torrent_multi_file_pieces(self):
+        piece_length = 8388608
+        golden_first = [(0, 41), (1, 1108), (2, 8387459)]
+        golden = [(2, piece_length)]
+        for piece in self.piece_manager.missing_pieces:
+            if piece.index == 0:
+                self.assertEqual(golden_first, piece.files)
+            else:
+                self.assertEqual(golden, piece.files)
+
+    def test_torrent_multi_file_files(self):
+        golden = [TorrentFile('Download more at www.ettvcentral.com .txt', 41, 1, 0),
+                  TorrentFile('Rick.and.Morty.S05E06.XviD-AFG.nfo', 1108, 1, 41),
+                  TorrentFile('Rick.and.Morty.S05E06.XviD-AFG[ettv].avi', 380672528, 46, 1149)]
+        self.assertEqual(golden, self.piece_manager.torrent.files)
+
+    def test_torrent_multi_file_file_descrptors(self):
+        self.assertEqual(3, len(self.piece_manager.fds))
